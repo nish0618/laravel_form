341060652c360ce1948021967c5d153db23432ad
nishijima yuma <nykh24yuuma@gmail.com>
2021-05-03 22:40:13 +0900

    管理画面修正

--- resources/views/admin/apply/index.blade.php
+++ resources/views/admin/apply/index.blade.php
@@ -38,7 +38,7 @@
 
                                     <div class="mx-2">
                                         <button type="submit" name="button" class="btn btn-sm btn-success" value="csv">
-                                            CSV
+                                            CSVダウンロード
                                         </button>
                                     </div>
                                 </div>
